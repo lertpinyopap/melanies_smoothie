62986d6ce8121472c5663c12c1a8a953acd082bf
yos.lertpinyopap <87996602+lertpinyopap@users.noreply.github.com>
2026-01-16 10:53:30 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -20,6 +21,9 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:'
                                  , max_selections=5
                                  )
 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
